c891a2e340682b9d668c2020b6fd718d85c9bd2b
Filip Gawlik <filippo.gawlik@gmail.com>
2026-01-12 20:02:39 +0100

    Added delay to random trading strategy

--- src/main/java/com/psio/trading/strategies/RandomTradingStrategy.java
+++ src/main/java/com/psio/trading/strategies/RandomTradingStrategy.java
@@ -7,16 +7,37 @@ import java.util.Random;
 
 public class RandomTradingStrategy implements TradingStrategy {
 
+    private final int delay;
+    private final Random random = new Random();
+
+    private int currentIndex = 0;
+
+    public RandomTradingStrategy() {
+        this.delay = 1;
+    }
+
+    public RandomTradingStrategy(int delay) {
+        this.delay = delay;
+    }
+
     @Override
     public TradingAction decide(MarketDataPayload marketDataPayload) {
-        int strat = new Random().nextInt(3);
+        currentIndex++;
 
-        return switch (strat) {
-            case 0 -> TradingAction.BUY;
-            case 1 -> TradingAction.SELL;
-            case 2 -> TradingAction.HOLD;
-            default -> throw new IllegalStateException("Unexpected value: " + strat);
-        };
+        if (currentIndex == delay) {
+            currentIndex = 0;
+
+            int strat = new Random().nextInt(3);
+
+            return switch (strat) {
+                case 0 -> TradingAction.BUY;
+                case 1 -> TradingAction.SELL;
+                case 2 -> TradingAction.HOLD;
+                default -> throw new IllegalStateException("Unexpected value: " + strat);
+            };
+        } else {
+            return TradingAction.BUY;
+        }
     }
 
     @Override
